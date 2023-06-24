64f738abcb446e08eb067ce0686f2597b1f5852a
Kfir <royalplusto@gmail.com>
2023-06-24 18:49:54 +0300

    init

---
+++ src/components/Mode.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { onMount } from 'svelte';
+  
+    let isDarkMode = false;
+  
+    // Check if dark mode is enabled on component mount
+    onMount(() => {
+      const isDarkModeEnabled = JSON.parse(localStorage.getItem('darkMode'));
+      if(isDarkModeEnabled === null) {
+        isDarkMode = true;
+      }
+      else if (isDarkModeEnabled) {
+        isDarkMode = true;
+      }
+      else {
+        document.documentElement.classList.remove('dark');
+        isDarkMode = false;
+      }
+    });
+  
+    function toggleDarkMode() {
+      isDarkMode = !isDarkMode;
+      if (isDarkMode) {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('darkMode', 'true');
+      } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('darkMode', 'false');
+      }
+    }
+</script>
+  
+
+<li>
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <i class="fa-solid fa-moon text-2xl cursor-pointer" on:click={toggleDarkMode}></i>
+</li>
---
+++ tailwind.config.cjs
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        "bk-dark-shades": "#153347",
+        "bk-dark-accent": "#194564",
+        "bk-main-brand": "#47729F",
+        "bk-light-accent": "#beccd1",
+        "bk-light-shades": "#E6E4E7",
+        "bkl-dark-shades": "#eee4dd",
+        "bkl-dark-accent": "#e6ba9b",
+        "bkl-main-brand": "#b88d60",
+        "bkl-light-accent": "#41332e",
+        "bkl-light-shades": "#191b18",
+      },
+      backgroundImage: {
+        "hero-bg": "url('/images/bg.jpeg')",
+      },
+    },
+    fontFamily: {
+      Poopins: ["Poopins, sans-serif"],
+      Bebas: ["Bebas Neue, sans-serif"],
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1600px",
+        xl: "1600px",
+        "2xl": "1600px",
+      },
+    },
+  },
+  plugins: [],
+};
