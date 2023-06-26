0fd69849d2bee09c7485e733c66325ac77271429
Kfir <royalplusto@gmail.com>
2023-06-26 12:17:18 +0300

    quick fix

--- src/components/MobileMenu.svelte
+++ src/components/MobileMenu.svelte
@@ -14,16 +14,14 @@
       class="fas fa-bars text-2xl dark:text-bk-light-shades text-bkl-light-shades cursor-pointer switch"
       on:click={toggleMenu}
     ></i>
-    <hr
-    class="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-bk-light-shades to-transparent opacity-25 dark:opacity-100" />
 </div>
 
 {#if !isHidden}
 <ul transition:fade class="dark:text-bk-light-shades text-bkl-light-shades border-y-[1px] dark:border-bk-light-accent border-bkl-light-accent flex flex-col absolute right-0 left-0 items-center top-24 dark:bg-bk-dark-shades bg-bkl-dark-shades py-12 gap-6 z-50 uppercase">
     <a href="/pricing" class="cursor-pointer hover:opacity-70 transition-all duration-200">Pricing</a>
-    <hr
-  class="my-12 h-px border-t-0 bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
+    <hr class="h-px w-11/12">
     <a href="/contact" class="cursor-pointer hover:opacity-70 transition-all duration-200">Contact</a>
+    <hr class="h-px w-11/12">
     <Mode />
 </ul>
 {/if}
